e8312337fcbd1fe80fa11cb63def613a4aad4cce
Ryan_Thomas <rthoma11@villanova.edu>
2016-12-05 10:47:43 -0800

    got board to click with x's

--- js/app.js
+++ js/app.js
@@ -2,5 +2,36 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  $("#board").on("click", function() {
+    box.one("click", play)
+
+
+  });
+
+  var box = $(".box");
+
+  var x = "X";
+  var o = "O";
+
+
+  var count = 0;
+
+  function play() {
+    if(count % 2 === 0) {
+      $(this).text(x);
+    } else {
+      $(this).text(0);
+    }
+  }
+
+  // function markX() {
+  //   //document.css(background-image: x picture)
+  //   console.log("X working");
+  // }
+  //
+  // function markO() {
+  //   console.log("O working");
+  //
+  // }
 
 });
